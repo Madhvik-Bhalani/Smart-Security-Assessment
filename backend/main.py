6b357af5f78df3db079c5fc971b3188b0adf9c06
Madhvik Bhalani <madhvikbhalaniak37@gmail.com>
2025-02-03 23:33:24 +0100

    update gunicorn

--- backend/main.py
+++ backend/main.py
@@ -71,6 +71,10 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
+@app.get("/")
+def read_root():
+    return {"Welcome to the Astra's API"}
+
 # Include user routes
 app.include_router(user_router, prefix="/api/v1/users")
 app.include_router(chat_router, prefix="/api/v1")
