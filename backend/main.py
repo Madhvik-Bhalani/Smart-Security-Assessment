248ec1b483b667b0b16d6e543e23b7f311757981
Darshan Dhanani <112533833+darshan-1611-dev@users.noreply.github.com>
2025-02-05 15:33:21 +0200

    Update main.py to change allow origin

--- backend/main.py
+++ backend/main.py
@@ -66,7 +66,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,  # Allow all origins
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
